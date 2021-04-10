a8a0603f4968d890097fab5b660faed0d8412347
adawong240598 <adawong240598@hotmail.com>
2021-04-11 03:46:08 +0800

    Still got error

--- src/app/header/header/header.component.ts
+++ src/app/header/header/header.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { MenuItem } from 'primeng/api';
+
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { SessionService } from '../../services/session.service';
+import { CompanyService } from 'src/app/services/company.service';
+
+import{CompanyEntity} from '../../models/company-entity';
 
 @Component({
   selector: 'app-header',
@@ -8,12 +15,26 @@ import { MenuItem } from 'primeng/api';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output()
+  childEvent = new EventEmitter();
+
+  email: string | undefined;
+  password: string | undefined;
+  loginError: boolean;
+  errorMessage: string | undefined;
+
   items: MenuItem[];
 
 
-  constructor() {
+  constructor(private router: Router,
+    private activatedRoute: ActivatedRoute,
+    public sessionService: SessionService,
+    private compamnyService: CompanyService) {
+    this.loginError = false;
     this.items = new Array();
   }
+
+
 
 
   ngOnInit(): void {
@@ -32,6 +53,46 @@ export class HeaderComponent implements OnInit {
       }
     ];
   }
+
+  staffLogin(): void
+	{
+		this.sessionService.setEmail(this.email);
+		this.sessionService.setPassword(this.password);
+					
+		this.companyService.login(this.email, this.password).subscribe(
+			response => {										
+				let company: CompanyEntity = response;
+				
+				if(company != null)
+				{
+					this.sessionService.setIsLogin(true);
+					this.sessionService.setCurrentStaff(staff);					
+					this.loginError = false;
+					
+					this.childEvent.emit();
+					
+					this.router.navigate(["/index"]);
+				}
+				else
+				{
+					this.loginError = true;
+				}
+			},
+			error => {
+				this.loginError = true;
+				this.errorMessage = error
+			}
+		);
+	}
+
+
+  companyLogout(): void
+	{
+		this.sessionService.setIsLogin(false);
+		this.sessionService.setCompany(null);
+		
+		this.router.navigate(["/index"]);
+	}
 
   getMenuItems(): MenuItem[] {
     return this.items;
