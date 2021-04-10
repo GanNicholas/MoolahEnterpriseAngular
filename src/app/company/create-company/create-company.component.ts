b17c09bd44c4e4d8e317815fc7ba70c4ec5dd0db
rayray1996 <e0417718@u.nus.edu>
2021-04-11 03:28:28 +0800

    first draft on create company component

--- src/app/company/create-company/create-company.component.ts
+++ src/app/company/create-company/create-company.component.ts
@@ -1,4 +1,13 @@
+import { CreateCompanyEntityReq } from './../../models/create-company-entity-req';
+import { CompanyEntity } from './../../models/company-entity';
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { CompanyService } from 'src/app/services/company.service';
+import { NgForm } from '@angular/forms';
+import { PasswordModule } from 'primeng/password';
+import {ButtonModule} from 'primeng/button';
+import { PointOfContactEntity } from 'src/app/models/point-of-contact-entity';
+
 
 @Component({
   selector: 'app-create-company',
@@ -7,9 +16,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateCompanyComponent implements OnInit {
 
-  constructor() { }
+  resultSuccess: boolean = false;
+  resultError: boolean = false;
+  submitted: boolean = true;
+  message: string | undefined;
+  createCompanyEntityReq: CreateCompanyEntityReq;
+
+  constructor(private router: Router,
+    private companyService: CompanyService) {
+    this.createCompanyEntityReq = new CreateCompanyEntityReq();
+  }
 
   ngOnInit(): void {
   }
 
+  create(createCompanyForm: NgForm) {
+
+    this.submitted = true;
+    if (createCompanyForm.valid) {
+      this.companyService.createNewCompany(this.createCompanyEntityReq).subscribe(
+        response => {
+          let newCompanyId: number = response;
+          this.resultSuccess = true;
+          this.resultError = false;
+          this.message = "Company " + newCompanyId + " created successfully";
+        },
+        error => {
+          this.resultError = true;
+          this.resultSuccess = false;
+          this.message = "An error has occurred while creating the new company: " + error;
+
+          console.log('********** CreateCompanyComponent.ts: ' + error);
+        }
+      );
+    }
+  }
+
+  handleClickAdd() {
+    this.createCompanyEntityReq?.listOfPointOfContacts?.push(new PointOfContactEntity());
+  }
+
+  handleClickRemove(index: number) {
+    this.createCompanyEntityReq?.listOfPointOfContacts?.splice(index, 1);
+  }
 }
--- src/app/models/create-company-entity-req.ts
+++ src/app/models/create-company-entity-req.ts
@@ -2,8 +2,8 @@ import { CompanyEntity } from "./company-entity";
 import { PointOfContactEntity } from "./point-of-contact-entity";
 
 export class CreateCompanyEntityReq {
-    companyEntity: CompanyEntity | undefined;
-    listOfPointOfContacts: PointOfContactEntity[] | undefined;
+    companyEntity: CompanyEntity | undefined = new CompanyEntity();
+    listOfPointOfContacts: PointOfContactEntity[] | undefined = new Array();
 
     constructor(companyEntity?: CompanyEntity, listOfPointOfContacts?: PointOfContactEntity[]) {
         this.companyEntity = companyEntity;
