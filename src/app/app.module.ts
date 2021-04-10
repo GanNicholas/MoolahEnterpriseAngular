915d5cfdc58386b767546d711051a1d557d4b102
adawong240598 <adawong240598@hotmail.com>
2021-04-11 01:50:57 +0800

    ada's second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,27 @@ import { HttpClientModule } from '@angular/common/http';
 import { TableModule } from 'primeng/table';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CreateCompanyComponent } from './create-company/create-company.component';
+import { CreateCompanyComponent } from './company/create-company/create-company.component';
+import { IndexComponent } from './index/index/index.component';
+import {MenubarModule} from 'primeng/menubar';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreateCompanyComponent
+    CreateCompanyComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    TableModule
+    TableModule,
+    MenubarModule,
+    
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/index/index/index.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import {MenuItem} from 'primeng/api';
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.css']
+})
+export class IndexComponent implements OnInit {
+
+  items: MenuItem[] | undefined;
+
+  constructor() {
+    this.items = new Array();
+  }
+   
+
+  ngOnInit(): void {
+    this.items = [
+      {
+          label: 'Who are we?',
+          items: [{
+                  label: 'New', 
+                  icon: 'pi pi-fw pi-plus',
+                  items: [
+                      {label: 'Project'},
+                      {label: 'Other'},
+                  ]
+              },
+              {label: 'Open'},
+              {label: 'Quit'}
+          ]
+      },
+      {
+          label: 'Edit',
+          icon: 'pi pi-fw pi-pencil',
+          items: [
+              {label: 'Delete', icon: 'pi pi-fw pi-trash'},
+              {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
+          ]
+      }
+  ];
+  }
+
+}
