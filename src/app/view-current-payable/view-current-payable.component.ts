c8842e1693d6733acf23f520240cd828439576e4
rayray1996 <e0417718@u.nus.edu>
2021-04-13 21:23:24 +0800

    fixed unpaid

--- src/app/view-current-payable/view-current-payable.component.ts
+++ src/app/view-current-payable/view-current-payable.component.ts
@@ -142,10 +142,31 @@ export class ViewCurrentPayableComponent implements OnInit {
   }
 
   getDateGeneratedString(): string {
-    if (this.monthlyPayments[this.selected].dateGenerated == null) {
+    let monthlyPaymentEntity: MonthlyPaymentEntity = this.monthlyPayments[this.selected];
+    if (monthlyPaymentEntity !== undefined) {
+      var dateGenerated = monthlyPaymentEntity.dateGenerated;
+    } else {
+      dateGenerated = null;
+    }
+    if (dateGenerated === null || dateGenerated === undefined) {
       return "";
     } else {
-      return this.getDateString(this.monthlyPayments[this.selected].dateGenerated);
+      return this.getDateString(monthlyPaymentEntity.dateGenerated);
+    }
+  }
+
+  getTotalPayableString(): number {
+    let monthlyPaymentEntity: MonthlyPaymentEntity = this.monthlyPayments[this.selected];
+    if (monthlyPaymentEntity !== undefined) {
+      var total = monthlyPaymentEntity.totalPayable;
+    } else {
+      total = undefined;
+    }
+
+    if (total === null || total === undefined) {
+      return 0;
+    } else {
+      return total;
     }
   }
 }
